a459aaf9a3bea002a4247bbc637563229f4cadd1
deepcoder <45561556+bhuyanamit986@users.noreply.github.com>
2021-05-28 19:44:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () =>  Math.floor(Math.random() * 10);
+const compareGuesses = (human, computer, target) => (Math.abs(human - target) <= Math.abs(computer - target)) ? true : false
+const updateScore = (winner) => {
+  if(winner === 'human') {
+    humanScore ++;
+  }
+  else {
+    computerScore ++;
+  }
+}
+const advanceRound = () => {
+  currentRoundNumber ++;
+}
